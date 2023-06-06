05a0987ec79ec7289bb400c924b38b1b1b269555
zayze-dev <zayze@aliyun.com>
2023-06-06 15:15:02 +0800

    add tmux config

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -3,3 +3,4 @@ ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/yabai ~/.config/yabai
 ln -s ~/.dotfiles/skhd ~/.config/skhd
 ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
