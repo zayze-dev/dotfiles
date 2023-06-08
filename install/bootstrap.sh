41080091d37f2eceac20fe36c07dd6ede0e151fa
zayze-dev <zayze@aliyun.com>
2023-06-08 14:33:41 +0800

    add kitty.conf

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -2,5 +2,6 @@
 ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
 ln -s ~/.dotfiles/yabai ~/.config/yabai
 ln -s ~/.dotfiles/skhd ~/.config/skhd
-ln -s ~/.dotfiles/alacritty ~/.config/alacritty
+#ln -s ~/.dotfiles/alacritty ~/.config/alacritty
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
+ln -s ~/.dotfiles/kitty ~/.config/kitty
