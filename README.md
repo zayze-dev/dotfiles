b35c1b8dfbcc8145fb7cd1f45c451fd10c6ddb95
zayze-dev <zayze@aliyun.com>
2023-06-11 17:02:23 +0800

    进行了很多修改

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ dotfiles created at 2023-06-06
 ```shell
 git clone https://github.com/zayze-dev/dotfiles.git ~/.dotfiles
 ```
+这里会有
+1. 整理配置文件的路径位置
+2. 常用alias
+
+这里不会有
+1. 区别与各个设备的本地设置
+2. 与配置相关的大文件，插件等等
--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -5,3 +5,6 @@ ln -s ~/.dotfiles/skhd ~/.config/skhd
 #ln -s ~/.dotfiles/alacritty ~/.config/alacritty
 ln -s ~/.dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -s ~/.dotfiles/kitty ~/.config/kitty
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/joshuto ~/.config/joshuto
+ln -s ~/.dotfiles/nvim ~/.config/nvime
