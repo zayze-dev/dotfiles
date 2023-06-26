8b9002986e7081b021c1b1ebbd77952241705543
zayze-dev <zayze@aliyun.com>
2023-06-26 18:49:13 +0800

    fix nvim folder name

--- install/bootstrap.sh
+++ install/bootstrap.sh
@@ -7,4 +7,4 @@ ln -s ~/.dotfiles/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -s ~/.dotfiles/kitty ~/.config/kitty
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s ~/.dotfiles/joshuto ~/.config/joshuto
-ln -s ~/.dotfiles/nvim ~/.config/nvime
+ln -s ~/.dotfiles/nvim ~/.config/nvim
